c8c01b0bfd52bf345ece00c43cb59186a0a0a3a9
Cristian <marquezprogramcristian@gmail.com>
2019-09-20 22:57:30 -0500

    New text for forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,9 +5,9 @@ const forecast = (lat, long, callback) => {
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('Unable to connect to forecast services', undefined)
+            callback('Unable to connect to forecast services. Please check internet connection.', undefined)
         } else if (body.error) {
-            callback('Coordinates are incorrect', undefined)
+            callback('Coordinates are incorrect. Please try again.', undefined)
         } else {
             callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees outside. There is a ${body.currently.precipProbability}% chance of rain`)
         }
